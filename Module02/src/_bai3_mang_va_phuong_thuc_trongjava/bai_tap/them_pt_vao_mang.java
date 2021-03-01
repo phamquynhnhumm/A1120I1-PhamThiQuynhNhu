b922b7054054032424b30c98b68962bef5dda4b3
unknown <quynhnhuctcn@gmail.com>
2021-03-01 18:15:05 +0700

    then_phan_tu_vao_mang

--- Module02/src/_bai3_mang_va_phuong_thuc_trongjava/bai_tap/them_pt_vao_mang.java
+++ Module02/src/_bai3_mang_va_phuong_thuc_trongjava/bai_tap/them_pt_vao_mang.java
@@ -1,4 +1,59 @@
 package _bai3_mang_va_phuong_thuc_trongjava.bai_tap;
 
+import java.util.Scanner;
+
 public class them_pt_vao_mang {
+    public static void main(String[] args) {
+        int n;
+        Scanner nhapn = new Scanner(System.in);
+        System.out.println("Nhập vào số phần tử trong mảng :");
+        n = nhapn.nextInt();
+        int[] a = new int[n];
+
+        Scanner nhapso = new Scanner(System.in);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("a[" + i + "] =");
+            a[i] = nhapso.nextInt();
+        }
+
+        System.out.print("Mảng là :");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        int them, sothem;
+        Scanner nhapthem = new Scanner(System.in);
+        System.out.print("\nNhập vào vị trí bạn muốn thêm");
+        them = nhapthem.nextInt();
+
+        Scanner nhapsothem = new Scanner(System.in);
+        System.out.print("Nhập vào số bạn muốn thêm");
+        sothem = nhapsothem.nextInt();
+
+
+        int [] b = new int[a.length +1];
+        for( int i=0; i<a.length ;i++)
+        {
+            b[i] = a[i];
+        }
+
+        for (int i = them; i <= a.length; i++) {
+            if (i == them) {
+
+                for (int j = b.length-1; j >=i+1 ; j--) {
+                    b[j] = b[j-1];
+                }
+                b[i] = sothem;
+            }
+        }
+        System.out.println("Mảng đã thêm là :");
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+    }
+    public  void  them()
+    {
+
+    }
+
 }
---
+++ Module02/src/_bai4_lop_va_doi_tuong_trong_java/bai_tap/Xd_lop_StopWatch.java
@@ -0,0 +1,4 @@
+package _bai4_lop_va_doi_tuong_trong_java.bai_tap;
+
+public class Xd_lop_StopWatch {
+}
