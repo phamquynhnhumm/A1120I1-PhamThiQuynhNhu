a609dae98d5dfd157eda12d965c2df18175d47a9
unknown <quynhnhuctcn@gmail.com>
2021-03-29 20:42:01 +0700

    up

--- Module02/src/_bai11_DSA_Stack_Queue/_bai_tap/demsolanxuathientutrong_Map.java
+++ Module02/src/_bai11_DSA_Stack_Queue/_bai_tap/demsolanxuathientutrong_Map.java
@@ -1,4 +1,17 @@
-package _bai11_DSA_Stack_Queue._bai_tap;
-
-public class demsolanxuathientutrong_Map {
-}
+//package _bai11_DSA_Stack_Queue._bai_tap;
+//
+//public class demsolanxuathientutrong_Map {
+//    public static void main(String[] args) {
+//        Map<Student, String> myMap = new TreeMap<>();
+//        myMap.put(new Student(1, "Nguyen Van A"), "Top1");
+//        myMap.put(new Student(2, "Nguyen Van B"), "Top3");
+//        myMap.put(new Student(4, "Nguyen Van C"), "Top4");
+//        myMap.put(new Student(3, "Nguyen Van D"), "Top5");
+//        myMap.put(null, "Top99");
+//
+//        for (Student key : myMap.keySet()) {
+//            System.out.println("key: " + key + " value: " + myMap.get(key));
+////        }
+//    }
+//
+//}
