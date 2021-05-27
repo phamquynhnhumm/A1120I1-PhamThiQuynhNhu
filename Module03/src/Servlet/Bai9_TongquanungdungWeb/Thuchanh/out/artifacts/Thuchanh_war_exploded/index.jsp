520b5458eee9090cf6cedc025123162de95064de
unknown <quynhnhuctcn@gmail.com>
2021-05-27 23:08:36 +0700

    thoigianthuc

--- Module03/src/Servlet/Bai9_TongquanungdungWeb/Thuchanh/out/artifacts/Thuchanh_war_exploded/index.jsp
+++ Module03/src/Servlet/Bai9_TongquanungdungWeb/Thuchanh/out/artifacts/Thuchanh_war_exploded/index.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: nhu
   Date: 5/27/2021
-  Time: 10:53 PM
+  Time: 11:04 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
--- Module03/src/Servlet/Bai9_TongquanungdungWeb/Thuchanh/src/ThoiGianThuc.java
+++ Module03/src/Servlet/Bai9_TongquanungdungWeb/Thuchanh/src/ThoiGianThuc.java
@@ -1,14 +1,15 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
 
-@WebServlet(name = "datetime",urlPatterns = "/index")
-public class datetime extends HttpServlet {
+import javax.servlet.ServletException;
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
+        import java.io.PrintWriter;
+        import java.util.Date;
+
+@WebServlet(name = "ThoiGianThuc",urlPatterns = "/thoi-gian-thuc")
+public class ThoiGianThuc extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
