c2e47f2d683cf857a350f05318d9ed78fbee204b
unknown <quynhnhuctcn@gmail.com>
2021-06-08 00:04:52 +0700

    ungdungquanlykhachhang

--- Module03/src/Servlet/Bai11_MVC_Model/thuchanh/java_web_mvc/src/service/CustomerService.java
+++ Module03/src/Servlet/Bai11_MVC_Model/thuchanh/java_web_mvc/src/service/CustomerService.java
@@ -1,4 +1,14 @@
 package service;
 
+import model.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
+    List<Customer> findAll();
+    void save(Customer customer);
+    Customer finById(int id);
+    void update(int id, Customer customer);
+    void remove(int id);
+
 }
