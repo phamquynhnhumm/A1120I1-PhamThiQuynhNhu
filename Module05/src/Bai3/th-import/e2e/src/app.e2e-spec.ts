9f2cd291ad2ffb5713cd5d10d2e969f06ce6a919
nhu <quynhnhuctcn@gmail.com>
2021-11-19 10:20:06 +0700

    import boostrap

--- Module05/src/Bai3/th-import/e2e/src/app.e2e-spec.ts
+++ Module05/src/Bai3/th-import/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('th-thongtinthunuoi app is running!');
+    expect(page.getTitleText()).toEqual('th-import app is running!');
   });
 
   afterEach(async () => {
