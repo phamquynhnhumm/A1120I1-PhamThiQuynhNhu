335f872b5deea2f970a758be7f38a677849a8271
nhu <quynhnhuctcn@gmail.com>
2021-11-22 20:15:55 +0700

    Progress Bar

--- Module05/src/Bai5Interaction/bai5/src/app/app.module.ts
+++ Module05/src/Bai5Interaction/bai5/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameCardComponent } from './name-card/name-card.component';
+import { PrigressBarComponent } from './prigress-bar/prigress-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NameCardComponent
+    NameCardComponent,
+    PrigressBarComponent
   ],
   imports: [
     BrowserModule,
